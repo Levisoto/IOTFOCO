fa68e12c7d4ab5664776fd189be4a6dbcb47297c
LaxusxN <44536278+LaxusxN@users.noreply.github.com>
2018-12-20 14:17:01 -0500

    Update controlfocoIOT.ino

--- controlfocoIOT.ino
+++ controlfocoIOT.ino
@@ -7,7 +7,6 @@
 #define WIFI_PASSWORD ""
 #define LED D0            // Led in NodeMCU at pin GPIO16 (D0).
 
-float suma;
 void setup() {
   // put your setup code here, to run once:
 
@@ -30,18 +29,16 @@ int n=0;
 
 void loop() {
   // put your main code here, to run repeatedly:
-//suma=suma+1;
 n=Firebase.getInt("LIGHT_STATUS");
-
 
     if (n==1) {
     digitalWrite(D0,LOW); 
     delay(10);
-}
+              }
 
-if (n==0) {
+    if (n==0) {
     digitalWrite(D0,HIGH); 
     delay(10);
 
-}
+              }
 }
