6b08c988e511cc3ad04409a9a47d32972ed06e1e
LaxusxN <luiscalagua2294@gmail.com>
2019-01-02 10:01:18 -0500

    changing multiwifi for wifibegin. Reconnection solve

--- controlfocoIOT/controlfocoIOT.ino
+++ controlfocoIOT/controlfocoIOT.ino
@@ -39,7 +39,7 @@ WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
   Serial.println("Connected");
   Serial.println(WiFi.localIP());
 
-
+  
   connectioWasAlive = false;
 wifiMulti.addAP("Laxus", "illdoit1");
 }
@@ -47,7 +47,93 @@ wifiMulti.addAP("Laxus", "illdoit1");
 void loop() {
 WiFiClientSecure client;
 
-if(wifiMulti.run() != WL_CONNECTED ){
+
+if(WiFi.status() != WL_CONNECTED ){
+  
+    Serial.print(".");
+    if (connectioWasAlive == true)
+    {
+      connectioWasAlive = false;
+      WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
+      Serial.print("Looking for WiFi.");
+    }
+    Serial.print(".");
+    delay(500);
+  }
+ else{
+  if (connectioWasAlive == false || connection == true)
+  {
+   if(connection == false || connectioWasAlive == false){
+      connection = true;
+      Serial.printf(" connected to %s\n", WiFi.SSID().c_str());     
+      Serial.println("true");
+    }else{
+      
+      Serial.print("connecting to ");
+      Serial.println(host);
+      if (client.connect(host,httpsPort)) {
+        if (client.verify(fingerprint, host)) {
+            Serial.println("certificate matches");
+            } else {
+            Serial.println("certificate doesn't match");
+        }
+          Serial.println("connected]");
+          String url = "/api/lightStatus";
+          Serial.println("[Sending a request]");
+          client.print(String("GET /") + " HTTP/1.1\r\n" +
+                 "Host: " + host + "\r\n" +
+                 "Connection: close\r\n" +
+                 "\r\n"
+                );
+         }
+         else { Serial.println("Error with server"); return;}
+      unsigned long timeout = millis();
+      while (client.available() == 0) {
+        if (millis() - timeout > 5000) {
+          Serial.println(">>> Client Timeout !");
+          client.stop();
+          delay(60000);
+          return;
+        }
+      }
+
+      // Read all the lines of the reply from server and print them to Serial
+      Serial.println("receiving from remote server");
+      // not testing 'client.connected()' since we do not need to send data here
+      while (client.available()) {
+        char endOfHeaders[] = "\r\n\r\n";
+        client.find(endOfHeaders);
+
+        StaticJsonBuffer<100> jsonBuffer;
+        JsonObject& root = jsonBuffer.parseObject(client);
+        //root.prettyPrintTo(Serial);
+
+        int TPS = root["lightStatus"];
+        Serial.println(TPS);
+
+        switch (TPS){
+         case 0:  {digitalWrite(D0,LOW);  Serial.print("LOW");break;}
+         case 1: {digitalWrite(D0,HIGH); Serial.print("HIGHT");break;}
+        }
+
+      }
+
+      // Close the connection
+      Serial.println();
+      Serial.println("closing connection");
+      client.stop();
+
+      delay(1000); // execute once every 5 minutes, don't flood remote service
+
+      }
+      connectioWasAlive = true;
+    }
+    
+  }
+
+
+
+ /*if(wifiMulti.run() != WL_CONNECTED ){
   
     Serial.print(".");
     if (connectioWasAlive == true)
@@ -110,8 +196,8 @@ if(wifiMulti.run() != WL_CONNECTED ){
         Serial.println(TPS);
 
         switch (TPS){
-         case 1:  {digitalWrite(D0,LOW);  Serial.print("LOW");break;}
-         case 0: {digitalWrite(D0,HIGH); Serial.print("HIGHT");break;}
+         case 0:  {digitalWrite(D0,LOW);  Serial.print("LOW");break;}
+         case 1: {digitalWrite(D0,HIGH); Serial.print("HIGHT");break;}
         }
 
       }
@@ -127,5 +213,5 @@ if(wifiMulti.run() != WL_CONNECTED ){
       connectioWasAlive = true;
     }
     
-  }
+  }*/
 }
